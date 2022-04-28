e0b3d76881916b8bcacc5bf9583b823311100ba1
ftanevski <tanevski.f98@gmail.com>
2022-04-28 22:24:08 +0200

    It's all downhill from here

---
+++ src/test/java/com/example/frsystem/FrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
